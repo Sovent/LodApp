f72dd48410e8616c2d5adfa1f4de6645d69bcbe1
Sovent <vmargelov@gmail.com>
2018-05-16 02:01:17 +0300

    Добавил рендерер для кастомного контрола на iOS

---
+++ LodApp/LodApp.iOS/Renderers/FlexibleEditorRenderer.cs
@@ -0,0 +1,21 @@
+﻿using LodApp.Extensions;
+using LodApp.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(FlexibleEditor), typeof(FlexibleEditorRenderer))]
+namespace LodApp.iOS.Renderers
+{
+	public class FlexibleEditorRenderer : EditorRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				Control.ScrollEnabled = false;
+			}
+		}
+	}
+}
